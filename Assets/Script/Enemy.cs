24987cb45c71117fd00256fbf833cb520c84604c
bnbn131 <40466659+bnbn131@users.noreply.github.com>
2020-11-12 20:36:33 +0300

    Добавлен проект

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Animator anim;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            collision.gameObject.GetComponent<Player>().RecountHp(-1);
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 8f, ForceMode2D.Impulse);
+           
+
+        }
+    }
+
+ 
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public float jumpHeight;
+    public Transform groundCheck;
+    bool isGrounded;
+    Animator anim;
+    int curHP;
+    int maxHP = 3;
+
+    public Main main;
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        curHP = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckGround();
+       
+        
+
+        if (Input.GetAxis("Horizontal") == 0 && (isGrounded))
+        {
+            anim.SetInteger("State", 1);
+        }
+        else {
+            Flip();
+            if (isGrounded) {
+                anim.SetInteger("State", 2);
+            }
+        }
+
+      
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+
+    }
+
+    void Flip() {
+        if (Input.GetAxis("Horizontal") > 0)
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            
+        if (Input.GetAxis("Horizontal") < 0)
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+    }
+
+    void CheckGround() {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f);
+        isGrounded = colliders.Length > 1;
+        if (!isGrounded) {
+            anim.SetInteger("State", 3);
+        }
+    }
+
+    public void RecountHp(int deltaHP) {
+
+        curHP = curHP + deltaHP;
+      //  if(deltaHP < 0)
+            //anim.SetInteger("State", 4);
+        if (curHP <= 0) {
+           
+            GetComponent<BoxCollider2D>().enabled = false;
+            Invoke("Lose", 1f); 
+            
+
+        }
+
+    }
+
+    void Lose() {
+        main.GetComponent<Main>().Lose();
+    }
+
+   
+}
